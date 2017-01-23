de0d4a65642aa1fa388613eef029c93c0e335235
fr3ddy4 <fr3ddy4@yandex.ru>
2017-01-23 11:56:32 +0300

    #6. Choosing the right implementation added

--- src/main/java/ru/itis/inform/store/services/StoreServiceImpl.java
+++ src/main/java/ru/itis/inform/store/services/StoreServiceImpl.java
@@ -6,14 +6,24 @@ import ru.itis.inform.store.dao.ItemsDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.*;
+
 public class StoreServiceImpl implements StoreService {
     private Logger logger;
     private ItemsDao itemsDao;
+    private File file;
+    private BufferedReader reader;
 
-    public StoreServiceImpl(ItemsDao itemsDao) {
+    public StoreServiceImpl(ItemsDao itemsDao, String propertiesPath) {
+        file = new File(propertiesPath);
+        initializeReader();
         PropertyConfigurator.configure("src/main/java/ru/itis/inform/store/services/log4j.properties");
         logger = LoggerFactory.getLogger(StoreServiceImpl.class);
-        this.itemsDao = itemsDao;
+        try {
+            this.itemsDao = (ItemsDao) Class.forName(reader.readLine()).newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void buy(String itemName) {
@@ -29,6 +39,14 @@ public class StoreServiceImpl implements StoreService {
     public void delete(String itemName) {
         logger.info(itemName + " was deleted.");
         itemsDao.delete(itemName);
+    }
+
+    private void initializeReader(){
+        try {
+            reader = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
 
